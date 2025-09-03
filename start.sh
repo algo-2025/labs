c5a728bcfdcdbce6b85bdc89634e2b5a40effe50
Mawwlle <dunaev.maksim.03@bk.ru>
2025-09-03 17:27:06 +0300

    feat: simplified script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read prefix repo branch; do
+    echo "Добавляем лабораторную $prefix... $prefix из $repo ($branch)"
+    git subtree add --prefix="$prefix" "$repo" "$branch" --squash
+    echo "Лабораторная $prefix добавленa!"
+done < .subtrees
+
